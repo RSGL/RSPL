c87aa94a9e1a5459676f64b08ecde557cc64d6be
Colleague Riley <colleagueRiley@gmail.com>
2022-07-24 18:40:59 -0400

    fixed misspelled vars

--- RSPL.hpp
+++ RSPL.hpp
@@ -3,23 +3,23 @@
 #endif
 
 namespace RSGL{
-    enum enitytype{ platform, object };
-    struct enity{
-        enitytype et;
+    enum entitytype{ platform, object };
+    struct entity{
+        entitytype et;
         Shape shape;
 	bool water=false; int Float=0;
-        enity(RSGL::rect rect,enitytype type ){ et=type; r=rect; shape=RECT; }
-        enity(RSGL::circle circle,enitytype type ){ et=type; c=circle; shape=CIRCLE; }
-        enity(RSGL::triangle triangle,enitytype type ){ et=type; t=triangle; shape=TRIANGLE; }
-        enity(std::array<RSGL::point, 2> line,enitytype type ){ et=type; l=line; shape=LINE; }
-        bool collide(std::vector<enity> es, RSGL::point dir={0,0});
-	bool Collide(std::vector<enity> es, RSGL::point dir={0,0});
+        entity(RSGL::rect rect,entitytype type ){ et=type; r=rect; shape=RECT; }
+        entity(RSGL::circle circle,entitytype type ){ et=type; c=circle; shape=CIRCLE; }
+        entity(RSGL::triangle triangle,entitytype type ){ et=type; t=triangle; shape=TRIANGLE; }
+        entity(std::array<RSGL::point, 2> line,entitytype type ){ et=type; l=line; shape=LINE; }
+        bool collide(std::vector<entity> es, RSGL::point dir={0,0});
+	bool Collide(std::vector<entity> es, RSGL::point dir={0,0});
 
-	bool fall(std::vector<enity> es){
+	bool fall(std::vector<entity> es){
   	    if (jumping && !jFall) return false;
             if (et == platform) return false;
             bool collide=false;
-	    static int counter=0;
+	        static int counter=0;
             for (int i=0; i < es.size(); i++){
                 if (collide) return false;
                 switch(shape){
@@ -50,7 +50,7 @@ namespace RSGL{
 		if (!jumping) jump=0;
 		if (jump < jumpLimit) { jFall=false; jump++; jVertexY=vertexY; jumping=true; }
 	}
-	bool jump(std::vector<enity> es){
+	bool jump(std::vector<entity> es){
 		if (!jumping) return false;
 		static int y=0;
 		if (y == jVertexY){ jFall=true; y=0; }
@@ -59,7 +59,7 @@ namespace RSGL{
 		y++;
 		return true;
 	}
-	int ifCollide(std::vector<enity> es, RSGL::point dir,int val){ return collide(es,dir) ? 0 : val; }
+	int ifCollide(std::vector<entity> es, RSGL::point dir,int val){ return collide(es,dir) ? 0 : val; }
 	bool jumping=false,jFall=false; int jVertexY;
         RSGL::rect r;
         RSGL::circle c;
@@ -68,7 +68,7 @@ namespace RSGL{
     };
 };
 
-bool RSGL::enity::collide(std::vector<RSGL::enity> es, RSGL::point dir){
+bool RSGL::entity::collide(std::vector<RSGL::entity> es, RSGL::point dir){
     bool collide=false;
         for (int i=0; i < es.size(); i++){
     if (collide) return true;
@@ -94,4 +94,4 @@ bool RSGL::enity::collide(std::vector<RSGL::enity> es, RSGL::point dir){
         return collide;
 }
 
-bool RSGL::enity::Collide(std::vector<RSGL::enity> es, RSGL::point dir){ collide(es,dir); }
+bool RSGL::entity::Collide(std::vector<RSGL::entity> es, RSGL::point dir){ collide(es,dir); }
